55c802685070b9c9584d2236e0597d192b6ad042
Morgann05 <morganlemos05@gmail.com>
2025-01-28 18:42:19 -0300

    Update sheet1.js

--- public/js/sheet1.js
+++ public/js/sheet1.js
@@ -93,14 +93,14 @@ function resolveSuccessType(num, roll, showBranches) {
             return { description: 'Desastre', isSuccess: false };
         if (roll === 20)
             return { description: 'Perfeito', isSuccess: true };
-        if (roll <= num * extremeRate)
+        if (roll >= num * extremeRate)
             return { description: 'Extremo', isSuccess: true };
-        if (roll <= num * goodRate)
+        if (roll >= num * goodRate)
             return { description: 'Bom', isSuccess: true };
     }
-    if (roll <= num)
+    if (roll >= num)
         return { description: 'Sucesso', isSuccess: true };
-    if (roll > num)
+    if (roll < num)
         return { description: 'Fracasso', isSuccess: false };
 
     return { description: 'Unknown', isSuccess: false };
